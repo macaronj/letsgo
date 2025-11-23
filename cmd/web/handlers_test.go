d057ae33b6a9f923b6d18d8e80d2c1bb4b79930c
Macaronj <74501326+macaronj@users.noreply.github.com>
2025-11-23 17:56:29 +0100

    chapter_16.3_test_snippet_create_handler

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -196,3 +196,38 @@ func TestUserSignup(t *testing.T) {
 		})
 	}
 }
+
+// INFO: Exercise 16.3 end-to-end test for snippet create
+func TestSnippetCreate(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+
+	defer ts.Close()
+
+	// Test if unauthenticated user gets redirected to login screen
+	t.Run("Unauthenticated", func(t *testing.T) {
+		code, headers, _ := ts.get(t, "/snippet/create")
+
+		assert.Equal(t, code, http.StatusSeeOther)
+		assert.Equal(t, headers.Get("Location"), "/user/login")
+	})
+
+	// Test if mock user can log in
+	// FIX: Test fails
+	t.Run("Authenticated", func(t *testing.T) {
+		_, _, body := ts.get(t, "/user/login")
+		csrfToken := extractCSRFToken(t, body)
+
+		form := url.Values{}
+		form.Add("email", "alice@example.com")
+		form.Add("password", "pa$$word")
+		form.Add("csrf_token", csrfToken)
+		ts.postForm(t, "/user/login", form)
+	})
+
+	code, _, body := ts.get(t, "/snippet/create")
+
+	assert.Equal(t, code, http.StatusOK)
+	assert.StringContains(t, body, "<form action='/snippet/create' method='POST'>")
+}
